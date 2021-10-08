f724485d1eb4e4612a3e4f851755cf0fc09dc6a9
Alexis BECHET <abechet@e3r5p15.42.fr>
2021-10-08 16:23:11 +0200

    Input Field Melange

---
+++ Assets/Scripts/SelectFace.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectFace : MonoBehaviour
+{
+    private CubeState cubeState;
+    private ReadCube readCube;
+    int layerMask = 1 << 8;
+
+    private void Start()
+    {
+        cubeState = FindObjectOfType<CubeState>();
+        readCube = FindObjectOfType<ReadCube>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !CubeState.autoRotating)
+        {
+            readCube.ReadState();
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f, layerMask))
+            {
+                GameObject face = hit.collider.gameObject;
+                List<List<GameObject>> cubeSides = new List<List<GameObject>>()
+                {
+                    cubeState.up,
+                    cubeState.down,
+                    cubeState.left,
+                    cubeState.right,
+                    cubeState.front,
+                    cubeState.back,
+                };
+
+                foreach (List<GameObject> cubeSide in cubeSides)
+                {
+                    if (cubeSide.Contains(face))
+                    {
+                        cubeState.PickUp(cubeSide);
+
+                        cubeSide[4].transform.parent.GetComponent<PivotRotation>().Rotate(cubeSide);
+                    }
+                }
+            }
+        }
+    }
+}
